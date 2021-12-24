796c9650f138dc8fdfebf6ba486337d08253b64b
mamadou42 <momocoly42@gmail.com>
2021-12-24 15:51:13 +0000

    first commit

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from "uuid"
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({children}) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This is feedback item 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 8,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7,
+    },
+  ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
+
+  // Delete Feedback
+  const deleteFeedBack = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem} : item))
+  }
+
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    newFeedback.id = +newFeedback
+    setFeedback([newFeedback, ...feedback])
+  }
+  
+  return <FeedbackContext.Provider value={{ 
+    feedback,
+    deleteFeedBack,
+    addFeedback,
+    editFeedback,
+    feedbackEdit,
+    updateFeedback
+   }}>
+    {children}
+  </FeedbackContext.Provider>
+}
+
+export default FeedbackContext;
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum necessitatibus voluptates et delectus beatae, temporibus sunt eveniet tempore? Iure numquam provident repellat vel assumenda, voluptatem soluta officiis corporis sapiente nesciunt?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum necessitatibus voluptates et delectus beatae, temporibus sunt eveniet tempore? Iure numquam provident repellat vel assumenda, voluptatem soluta officiis corporis sapiente nesciunt?',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum necessitatibus voluptates et delectus beatae, temporibus sunt eveniet tempore? Iure numquam provident repellat vel assumenda, voluptatem soluta officiis corporis sapiente nesciunt?',
+  },
+]
+
+export default FeedbackData
